a33d5ffd434df1731e19b8f0b1d64d87931dd64d
Wa1t5 <wa1t5@proton.me>
2022-07-29 21:35:58 -0300

    [Window] Added SetWindowTitle(string title)

--- Include/Window.hpp
+++ Include/Window.hpp
@@ -5,6 +5,7 @@ class Window
 {
 public:
     Window(std::string title, int width, int height);
+    void SetWindowTitle(std::string title);
 
 public:
     SDL_Window* _window = nullptr;
--- Src/Framework/Window.cpp
+++ Src/Framework/Window.cpp
@@ -4,3 +4,8 @@ Window::Window(std::string title, int width, int height)
 {
     _window = SDL_CreateWindow(title.c_str(), 100, 100, width, height, 0);
 }
+
+void Window::SetWindowTitle(std::string title)
+{
+    SDL_SetWindowTitle(_window, title.c_str());
+}
